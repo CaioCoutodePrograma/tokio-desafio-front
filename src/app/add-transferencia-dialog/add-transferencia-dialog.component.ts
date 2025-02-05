9b3406f13c0999ae87f59aa760b932b0c8c7c1f5
Caio Couto de Farias <caiocoutofarias@gmail.com>
2025-02-05 16:13:02 -0300

    criado popup transferencia

--- src/app/add-transferencia-dialog/add-transferencia-dialog.component.ts
+++ src/app/add-transferencia-dialog/add-transferencia-dialog.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { TransferenciaModel } from '../model/transferencia.model';
 
 @Component({
   selector: 'app-add-transferencia-dialog',
   standalone: false,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   
   templateUrl: './add-transferencia-dialog.component.html',
   styleUrl: './add-transferencia-dialog.component.css'
 })
 export class AddTransferenciaDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<AddTransferenciaDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: TransferenciaModel
+  ) {}
+
+  
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onSave(): void{
+    this.dialogRef.close();
+  }
 
 }
